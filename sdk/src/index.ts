091904dcd5b8a04ce8407d5a0f14a0c02e85c594
Davi Cardoso <davicardoso.2103@gmail.com>
2024-01-25 00:45:38 -0300

    feat: add comment

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -77,6 +77,9 @@ export default class TriadClient {
     }))
   }
 
+  /**
+   * Get vault by name
+   */
   async getVaultByName(vaultName: string) {
     const encodeVaultName = encodeName(vaultName)
     const VaultPDA = getVaultAddressSync(
